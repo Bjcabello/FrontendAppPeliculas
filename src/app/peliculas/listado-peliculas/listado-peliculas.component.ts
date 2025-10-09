6214f32b3036252893c2c87a909800b32cc10369
Bryan Joel Cabello Mero <bjcabello55@gmail.com>
2025-10-09 09:12:14 -0500

    agregando angular material boton e iconos

--- src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
+++ src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -1,17 +1,25 @@
 import { CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
 import { Component, Input, input, OnInit } from '@angular/core';
 import { ListadoGenericoComponent } from "../../compartidos/componentes/listado-generico/listado-generico.component";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-listado-peliculas',
-  imports: [DatePipe, UpperCasePipe, CurrencyPipe, ListadoGenericoComponent],
+  imports: [
+    DatePipe,
+    UpperCasePipe,
+    CurrencyPipe,
+    ListadoGenericoComponent,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './listado-peliculas.component.html',
   styleUrl: './listado-peliculas.component.css',
 })
 export class ListadoPeliculasComponent {
   @Input({ required: true })
   peliculas!: any[];
-
 
   agregarPelicula() {
     this.peliculas.push({
@@ -23,8 +31,9 @@ export class ListadoPeliculasComponent {
     });
   }
   removerPelicula(pelicula: any) {
-  const indice = this.peliculas.findIndex(
-    (peliculaActual: any) => peliculaActual.titulo === pelicula.titulo);
-    this.peliculas.splice(indice,1)
+    const indice = this.peliculas.findIndex(
+      (peliculaActual: any) => peliculaActual.titulo === pelicula.titulo
+    );
+    this.peliculas.splice(indice, 1);
   }
 }
