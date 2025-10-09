21b21da1052fefb307d7092f5e812379426ea974
Bryan Joel Cabello Mero <bjcabello55@gmail.com>
2025-10-08 21:11:46 -0500

    agregando plantilla

--- src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
+++ src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -1,27 +1,30 @@
 import { CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
 import { Component, Input, input, OnInit } from '@angular/core';
+import { ListadoGenericoComponent } from "../../compartidos/componentes/listado-generico/listado-generico.component";
 
 @Component({
   selector: 'app-listado-peliculas',
-  imports: [DatePipe, UpperCasePipe, CurrencyPipe],
+  imports: [DatePipe, UpperCasePipe, CurrencyPipe, ListadoGenericoComponent],
   templateUrl: './listado-peliculas.component.html',
   styleUrl: './listado-peliculas.component.css',
 })
 export class ListadoPeliculasComponent {
-  
-  @Input({required:true})
+  @Input({ required: true })
   peliculas!: any[];
 
-  agregarPelicula(){
-    this,
-      this.peliculas.push({
-        titulo: 'Batman El Caballero de la Noche',
-        fechaLanzamiento: new Date('2025-10-07'),
-        precio: 15.00,
-        poster:
-          'https://m.media-amazon.com/images/M/MV5BN2U3NmZjMTYtY2JhOS00NzU4LWJkMDAtZjFmZjAyN2ZlMTMxXkEyXkFqcGc@._V1_.jpg',
-      });
+
+  agregarPelicula() {
+    this.peliculas.push({
+      titulo: 'Batman El Caballero de la Noche',
+      fechaLanzamiento: new Date('2025-10-07'),
+      precio: 15.0,
+      poster:
+        'https://m.media-amazon.com/images/M/MV5BN2U3NmZjMTYtY2JhOS00NzU4LWJkMDAtZjFmZjAyN2ZlMTMxXkEyXkFqcGc@._V1_.jpg',
+    });
   }
-
+  removerPelicula(pelicula: any) {
+  const indice = this.peliculas.findIndex(
+    (peliculaActual: any) => peliculaActual.titulo === pelicula.titulo);
+    this.peliculas.splice(indice,1)
+  }
 }
-
