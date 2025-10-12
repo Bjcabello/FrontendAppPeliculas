145e16666eac27bf2c29a77a6d65a0ea1b147928
Bryan Joel Cabello Mero <bjcabello55@gmail.com>
2025-10-11 19:45:59 -0500

    agregando componentes de editar para generos, peliculas, cines, actores

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,28 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { IndiceGenerosComponent } from './generos/indice-generos/indice-generos.component';
 import { CrearGenerosComponent } from './generos/crear-generos/crear-generos.component';
+import { IndiceActoresComponent } from './actores/indice-actores/indice-actores.component';
+import { CrearActorComponent } from './actores/crear-actor/crear-actor.component';
+import { IndiceCineComponent } from './cines/indice-cine/indice-cine.component';
+import { CrearCineComponent } from './cines/crear-cine/crear-cine.component';
+import { CrearPeliculaComponent } from './peliculas/crear-pelicula/crear-pelicula.component';
+import { EditarGeneroComponent } from './generos/editar-genero/editar-genero.component';
+import { EditarActorComponent } from './actores/editar-actor/editar-actor.component';
+import { EditarCineComponent } from './cines/editar-cine/editar-cine.component';
+import { EditarPeliculaComponent } from './peliculas/editar-pelicula/editar-pelicula.component';
 
 export const routes: Routes = [
-  {path:'', component:LandingPageComponent},
-  {path:'peliculas', component:LandingPageComponent},
-  {path:'generos', component:IndiceGenerosComponent},
-  {path:'generos/crear', component:CrearGenerosComponent},
+  { path: '', component: LandingPageComponent },
+  { path: 'generos', component: IndiceGenerosComponent },
+  { path: 'generos/crear', component: CrearGenerosComponent },
+  { path: 'generos/editar/:id', component: EditarGeneroComponent },
+  { path: 'actores', component: IndiceActoresComponent },
+  { path: 'actores/crear', component: CrearActorComponent },
+  { path: 'actores/editar/:id', component: EditarActorComponent },
+  { path: 'cine', component: IndiceCineComponent },
+  { path: 'cine/crear', component: CrearCineComponent },
+  { path: 'cine/editar/:id', component: EditarCineComponent },
+  { path: 'pelicula/crear', component: CrearPeliculaComponent },
+  { path: 'pelicula/editar/:id', component: EditarPeliculaComponent },
+  { path: '**', redirectTo: '' }
 ];
