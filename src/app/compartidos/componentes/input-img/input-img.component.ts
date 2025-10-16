7c92714ce56a6b62287b5b46e667415548ddc1d8
Bryan Joel Cabello Mero <bjcabello55@gmail.com>
2025-10-16 09:03:07 -0500

    ya agrega la foto

--- src/app/compartidos/componentes/input-img/input-img.component.ts
+++ src/app/compartidos/componentes/input-img/input-img.component.ts
@@ -1,26 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { toBase } from '../../funciones/toBase';
+
 
 @Component({
   selector: 'app-input-img',
   imports: [MatButtonModule],
   templateUrl: './input-img.component.html',
-  styleUrl: './input-img.component.css'
+  styleUrl: './input-img.component.css',
 })
 export class InputImgComponent {
-  @Input({required: true})
-   titulo!: string;
+  @Input({ required: true })
+  titulo!: string;
+  @Output()
+  archivoSeleccionado = new EventEmitter<File>();
 
   imagenBase?: string;
-  
-  cambio(event: Event){
+
+  cambio(event: Event) {
     const input = event.target as HTMLInputElement;
-    if(input.files && input.files.length > 0){
+    if (input.files && input.files.length > 0) {
       const file: File = input.files[0];
-      toBase(file).then((valor: string) => this.imagenBase = valor)
-      .catch(error => console.log());
+      toBase(file)
+        .then((valor: string) => {
+          this.imagenBase = valor;
+          console.log('imagenBase asignada:', this.imagenBase);
+        })
+        .catch((error) => console.error(error));
+        this.archivoSeleccionado.emit(file);
     }
   }
-
 }
