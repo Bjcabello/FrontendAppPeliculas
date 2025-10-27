1cb6f9d2329992dc52707b5e3d6d04c10d1a7a22
Bryan Joel Cabello Mero <bjcabello55@gmail.com>
2025-10-27 15:46:10 -0500

    actualizando genero

--- src/app/generos/editar-genero/editar-genero.component.ts
+++ src/app/generos/editar-genero/editar-genero.component.ts
@@ -1,20 +1,40 @@
-import { Component, Input, numberAttribute } from '@angular/core';
+import { Component, inject, Inject, Input, numberAttribute, OnInit } from '@angular/core';
 import { FormularioGeneroComponent } from "../formulario-genero/formulario-genero.component";
 import { GeneroCreacionDTO, GeneroDTO } from '../generos';
+import { GenerosService } from '../generos.service';
+import { CargandoComponent } from "../../compartidos/componentes/cargando/cargando.component";
+import { MostrarErroresComponent } from "../../compartidos/componentes/mostrar-errores/mostrar-errores.component";
+import { extraerErrores } from '../../compartidos/funciones/extraerErrores';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-editar-genero',
-  imports: [FormularioGeneroComponent],
+  imports: [FormularioGeneroComponent, CargandoComponent, MostrarErroresComponent],
   templateUrl: './editar-genero.component.html',
   styleUrl: './editar-genero.component.css'
 })
-export class EditarGeneroComponent {
+export class EditarGeneroComponent implements OnInit {
+  ngOnInit(): void {
+    this.generosService.obtenerPorId(this.id).subscribe(genero => {
+      this.genero = genero;
+    });
+  }
   @Input({transform: numberAttribute})
   id!: number ;
-
-  genero: GeneroDTO = {id: 1, nombre: 'Misterio'};
+  genero?: GeneroDTO ;
+  generosService = inject(GenerosService);
+  errores: string[] = [];
+  router = inject(Router);
 
   guardarCambios(genero: GeneroCreacionDTO){
-    console.log("Genero a editar: ", genero);
+    this.generosService.actualizar(this.id, genero).subscribe({
+      next: () => {
+        this.router.navigate(['/generos']);
+      },
+      error: (err) => {
+        const errores = extraerErrores(err);
+        this.errores = errores;
+      }
+    });
   }
 }
