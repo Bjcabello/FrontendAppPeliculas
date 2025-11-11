9be527defee3baa1e4dcaffbc43e9aa36599383e
Bryan Joel Cabello Mero <bjcabello55@gmail.com>
2025-11-11 11:43:32 -0500

    Componente de Ratings

--- src/app/peliculas/detalle-pelicula/detalle-pelicula.component.ts
+++ src/app/peliculas/detalle-pelicula/detalle-pelicula.component.ts
@@ -7,10 +7,14 @@ import { RouterLink } from '@angular/router';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Coordenada } from '../../compartidos/componentes/mapa/coordenada';
 import { MapaComponent } from "../../compartidos/componentes/mapa/mapa.component";
+import { RatingService } from '../../rating/rating.service';
+import Swal from 'sweetalert2';
+import { SeguridadService } from '../../seguridad/seguridad.service';
+import { RatingComponent } from "../../compartidos/componentes/rating/rating.component";
 
 @Component({
   selector: 'app-detalle-pelicula',
-  imports: [CargandoComponent, MatChipsModule, RouterLink, MapaComponent],
+  imports: [CargandoComponent, MatChipsModule, RouterLink, MapaComponent, RatingComponent],
   templateUrl: './detalle-pelicula.component.html',
   styleUrl: './detalle-pelicula.component.css'
 })
@@ -29,6 +33,8 @@ ngOnInit(): void {
 id!: number;
 
 peliculasService = inject(PeliculasService);
+ratingsService = inject(RatingService);
+seguridadService = inject(SeguridadService);
 pelicula!: PeliculaDTO;
 sanitizer = inject(DomSanitizer);
 trailerUrl!: SafeResourceUrl;
@@ -48,6 +54,16 @@ generarUrlYoutubeEmbed(url: string): SafeResourceUrl | string {
 
   return this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${videoId}`)
 
+}
+puntuar(puntuacion: number){
+  if(!this.seguridadService.estalogueado()){
+    Swal.fire('Error', 'Debes loguerte para poder votar por una pelicula', 'error');
+    return;
+  }
+
+  this.ratingsService.puntuar(this.id, puntuacion).subscribe(() =>{
+    Swal.fire('Exito', 'su voto ha sido recibido', 'success')
+  })
 }
 
 }
